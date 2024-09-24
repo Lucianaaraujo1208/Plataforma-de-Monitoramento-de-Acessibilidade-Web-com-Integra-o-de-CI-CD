a15acda1f2679dd2694298383f535938b68426e2
Luciana Araújo <83361295+Lucianaaraujo1208@users.noreply.github.com>
2024-09-24 13:20:39 -0300

    Adicionando os arquivos do projeto

--- cypress/acessibilidade.spec.js
+++ cypress/acessibilidade.spec.js
@@ -1,6 +1,6 @@
 describe('Teste de Acessibilidade', () => {
     it('Deve garantir que a página esteja acessível', () => {
-        // Visite a página que você deseja testar
+        // Visite a página que você deseja testar.
         cy.visit('https://github.com/Lucianaaraujo1208/Plataforma-de-Monitoramento-de-Acessibilidade-Web-com-Integra-o-de-CI-CD.git'); // Substitua pelo URL do seu site
         
         // Verifique a acessibilidade
